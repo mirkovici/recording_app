e2ef7242d538ab79db427c4c25d85f2a7c826984
Igor Mirkovic <igor.mirkovicc@gmail.com>
2024-06-13 19:08:16 +0200

    init

---
+++ client/src/components/RecordAudio.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="record_audio">Record audio</div>
+</template>
+
+<style scoped>
+.record_audio {
+  width: 200px;
+  height: 50px;
+  background-color: aliceblue;
+  color: darkgray;
+  text-align: center;
+  line-height: 50px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+</style>
